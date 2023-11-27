9376ab0db6a5ddd876bbc019ddf295ad81c1a0e5
Elias Fret <efret@a1-r2-p7.s19.be>
2023-11-27 12:21:09 +0100

    Now correctly handles malloc failure in ft_substr.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: efret <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:45:22 by efret             #+#    #+#             */
-/*   Updated: 2023/11/22 15:45:50 by efret            ###   ########.fr       */
+/*   Updated: 2023/11/27 12:04:10 by efret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,37 @@ static unsigned int	ft_count_words(const char *s, char c)
 	return (count);
 }
 
+static void	*ft_free_split(char **split, size_t end)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < end)
+		free(split[i++]);
+	free(split);
+	return (NULL);
+}
+
+static size_t	ft_fill_split(const char *s, char **word, char c, size_t *i)
+{
+	size_t	w_len;
+
+	w_len = 0;
+	while (s[w_len] && s[w_len] != c)
+		w_len++;
+	*word = ft_substr(s, 0, w_len);
+	if (!(*word))
+		return (0);
+	*i += w_len;
+	return (w_len);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	unsigned int	w_count;
-	unsigned int	i;
-	unsigned int	w_idx;
-	unsigned int	w_len;
-	char			**res;
+	size_t	w_count;
+	size_t	i;
+	size_t	w_idx;
+	char	**res;
 
 	w_count = ft_count_words(s, c);
 	res = malloc(sizeof(char *) * (w_count + 1));
@@ -47,15 +71,52 @@ char	**ft_split(char const *s, char c)
 	w_idx = 0;
 	while (s[i] && w_idx < w_count)
 	{
-		w_len = 0;
-		while (s[i + w_len] && (s[i + w_len] != c))
-			w_len++;
-		if (w_len)
-			res[w_idx++] = ft_substr(s, i, w_len);
-		i += w_len;
 		while (s[i] && (s[i] == c))
 			i++;
+		if (!ft_fill_split(&s[i], &res[w_idx++], c, &i))
+			return (ft_free_split(res, w_idx - 1));
 	}
 	res[w_idx] = 0;
 	return (res);
 }
+
+//#include <stdio.h>
+//int	main(void)
+//{
+//	char	*s = "   split me please   ";
+//	char	c = ' ';
+//	char	**split = ft_split(s, c);
+//	for (size_t i = 0; split[i]; i++)
+//		printf("%s\n", split[i]);
+//	ft_free_split(split, ft_count_words(s, c));
+//	return (0);
+//}
+
+//char	**ft_split(char const *s, char c)
+//{
+//	unsigned int	w_count;
+//	unsigned int	i;
+//	unsigned int	w_idx;
+//	unsigned int	w_len;
+//	char			**res;
+//
+//	w_count = ft_count_words(s, c);
+//	res = malloc(sizeof(char *) * (w_count + 1));
+//	if (!res)
+//		return (NULL);
+//	i = 0;
+//	w_idx = 0;
+//	while (s[i] && w_idx < w_count)
+//	{
+//		w_len = 0;
+//		while (s[i + w_len] && (s[i + w_len] != c))
+//			w_len++;
+//		if (w_len)
+//			res[w_idx++] = ft_substr(s, i, w_len);
+//		i += w_len;
+//		while (s[i] && (s[i] == c))
+//			i++;
+//	}
+//	res[w_idx] = 0;
+//	return (res);
+//}
